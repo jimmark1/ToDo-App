1fa146294efab78624094d66021b39f891350561
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-05 14:33:38 +0800

    Creating Task Added

--- todo_api/tasks_manager/urls.py
+++ todo_api/tasks_manager/urls.py
@@ -3,5 +3,4 @@ from . views import *
 
 urlpatterns = [
     path('', Tasks_manager.as_view(), name='tasks'),
-    
 ]
--- todo_api/tasks_manager/views.py
+++ todo_api/tasks_manager/views.py
@@ -6,10 +6,25 @@ from . models import *
 
 from . serializers import Tasks_serializer
 
-class Tasks_manager(APIView):
+class Tasks_scheduler():
+
+       def new_task(self, title, description, user):
+              Tasks.objects.create(
+                            task_title = title.upper(),
+                            task_description = description,
+                            user = user
+                     )
+
+
+       def update_task(self, pk):
+              pass
+
+
+       def delete_task(self, pk):
+              pass
+class Tasks_manager(APIView, Tasks_scheduler):
     
-       permission_classes = [permissions.IsAuthenticated
-]
+       permission_classes = [permissions.IsAuthenticated]
 
        def get(self, request, format=None):
               try:
@@ -24,12 +39,31 @@ class Tasks_manager(APIView):
                                      status=status.HTTP_400_BAD_REQUEST)
 
        def post(self, request):
+              data = request.data
+              task_title = data['task_title'].upper()
+
+              if len(data['task_title']) < 3 or data['task_title'].isspace():
+                     return Response({'error':'Task title should atleast 3 characters long'},
+                            status=status.HTTP_400_BAD_REQUEST)
+              
+              else:
+                     if Tasks.objects.filter(user=request.user, task_title = task_title).exists():
+                            return Response({'error':'Tasks already exists'},
+                                   status=status.HTTP_400_BAD_REQUEST)
+                     else:
+                            try:
+                                   self.new_task(title = data['task_title'].lstrip(), description = data['task_description'], user = request.user)
+                                   print('Task Title :', task_title)
+                                   return Response({'success':'Task added sucessfully'}, status=status.HTTP_201_CREATED)
+
+                            except Exception as e:
+                                   print(e)
+                                   return Response({'error':'Something went wrong while creating a task'},
+                                          status=status.HTTP_400_BAD_REQUEST)
+
+       def put(self, pk):
               pass
 
 
-       def put(self, request, pk):
-              pass
-
-
-       def delete(self, request, pk):
+       def delete(self, pk):
               pass

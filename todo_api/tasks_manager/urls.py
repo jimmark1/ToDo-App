7e99fd1a231d240714025b630e1ae20ab2b26ceb
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-05 12:35:47 +0800

    Task Manager App Added

--- todo_api/tasks_manager/urls.py
+++ todo_api/tasks_manager/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . views import *
 
 urlpatterns = [
-    path('', Tasks_manager.as_view(), name='task'),
+    path('', Tasks_manager.as_view(), name='tasks'),
     
 ]
--- todo_api/todo_api/urls.py
+++ todo_api/todo_api/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts_manager.urls')),
-    path('task/', include('tasks_manager.urls')),
+    path('tasks/', include('tasks_manager.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

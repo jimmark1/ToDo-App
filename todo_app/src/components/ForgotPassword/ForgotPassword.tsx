5d7bf4ad08a39fd2f2c3c16ef1f68f4c1624273e
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-10 21:32:11 +0800

    Forgot Password Added

---
+++ todo_app/src/components/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ForgotPassword = () => {
+     return (
+          <div>
+               <p>FORGOT PASSWORD PAGE</p>
+          </div>
+     );
+};
+
+export default ForgotPassword;

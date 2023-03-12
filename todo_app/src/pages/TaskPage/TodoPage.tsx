004f45cf8d6dd2c67bbdef423856db767d6e9d66
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-12 16:56:13 +0800

    Update TodoPage.tsx

--- todo_app/src/pages/TaskPage/TodoPage.tsx
+++ todo_app/src/pages/TaskPage/TodoPage.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+// import TodoForm from "../../components/Todo/TodoForm/TodoForm";
+// import TodoList from "../../components/Todo/TodoList/TodoList";
+
+import pageStlye from "../TaskPage/Todo.module.css";
+import "./pageStyles.css";
+
 const Todo = () => {
      return (
-          <div>
-               <p>This is Todo Page JUST DO IT</p>
+          <div
+               className={`${pageStlye.container} d-flex justify-content-center align-items-center`}>
+               <div className="col-lg-5 container-fluid">
+                    <Tabs
+                         className="nav-fill"
+                         defaultActiveKey="task-list"
+                         id="tab">
+                         <Tab
+                              className={`${pageStlye.tab}`}
+                              eventKey="task-list"
+                              title="task-list">
+                              <p>Tasks List</p>
+                         </Tab>
+                         <Tab
+                              eventKey="task-list-completed"
+                              title="task-list-completed">
+                              <p>Completed Tasks</p>
+                         </Tab>
+                    </Tabs>
+               </div>
           </div>
      );
 };

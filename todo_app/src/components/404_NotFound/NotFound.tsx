11f0656e8296ccd73a9864bdb9cffbf11a4a1bb7
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-12 13:15:36 +0800

    Page not Found Included

--- todo_app/src/components/404_NotFound/NotFound.tsx
+++ todo_app/src/components/404_NotFound/NotFound.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReturnSvgComponent } from "../Svgs/index";
 
 import NotFoundStyles from "../404_NotFound/NotFound.module.css";
-import ImageNotFound from "./Image/PageNotFound.jpg";
+import ImageNotFound from "./Images/PageNotFound.jpg";
 
 const NotFound = () => {
      return (
-          <div
-               className={`${NotFoundStyles.container} d-flex justify-content-center align-items-center`}>
-               <img
-                    className={`${NotFoundStyles.img}`}
-                    src={ImageNotFound}
-                    alt="404 Page Not Found"
-               />
+          <div className={`${NotFoundStyles.container} text-center`}>
+               <div className="d-flex justify-content-center align-items-center">
+                    <img
+                         className={`${NotFoundStyles.img}`}
+                         src={ImageNotFound}
+                         alt="404 Page Not Found"
+                    />
+               </div>
+               <div className="data-link">
+                    <Link className={`${NotFoundStyles.dataLink}`} to={"/"}>
+                         <img src={ReturnSvgComponent} alt="Return" />{" "}
+                         <span> Back to Homepage</span>
+                    </Link>
+               </div>
           </div>
      );
 };

09b9bd52e9dc0f10cbf0cd62a650a84c4f276359
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-24 21:39:09 +0800

    Update TodoList.tsx

--- todo_app/src/components/Todo/TodoList/TodoList.tsx
+++ todo_app/src/components/Todo/TodoList/TodoList.tsx
@@ -1,48 +1,14 @@
-import React, { useEffect, useState, useContext } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
 
 import ListGroup from "react-bootstrap/ListGroup";
-
-import { AuthContext } from "../../../contexts/AuthContext";
+import { TodoContext } from "../../../contexts/TodoContext";
 
 const TodoList = () => {
-     interface Todo {
-          readonly task_id: string;
-          task_title: string;
-          is_completed: boolean;
-          created_at: string;
-     }
-
-     let authContext = useContext(AuthContext);
-     const [todos, setTodos] = useState<Todo[]>([]);
-     const get_todos = async () => {
-          const request_instance = axios.create({
-               headers: {
-                    Authorization: `Bearer ${authContext?.authTokens?.access}`,
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-               },
-          });
-
-          const response = await request_instance.get(
-               "http://127.0.0.1:8000/tasks/",
-          );
-
-          if (response.status === 200) {
-               setTodos(response.data);
-          } else {
-               console.log(response);
-          }
-     };
-
-     useEffect(() => {
-          get_todos();
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-     }, []);
+     let todoContext = useContext(TodoContext);
 
      return (
           <>
-               {todos.map((todo) => (
+               {todoContext?.todos.map((todo) => (
                     <ListGroup variant="flush" key={todo.task_id}>
                          <ListGroup.Item>{todo.task_title}</ListGroup.Item>
                     </ListGroup>

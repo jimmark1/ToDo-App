2cdf1fdb54f8796e09d7afd3a793d850f6eef0c1
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-26 09:39:20 +0800

    Update TodoForm.tsx

--- todo_app/src/components/Todo/TodoForm/TodoForm.tsx
+++ todo_app/src/components/Todo/TodoForm/TodoForm.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import { AddSvg } from "../../Svgs";
 const TodoForm = () => {
      return (
-          <div className="">
-               <form action="">
-                    <p>form</p>
-               </form>
+          <div className="border-bottom border-secondary-subtle info">
+               <div className="mb-2">
+                    <Form>
+                         <div className="new-task d-flex gap-1">
+                              <Form.Control
+                                   type="text"
+                                   placeholder="Add New Task"></Form.Control>
+                              <Button variant="primary" type="submit">
+                                   <img src={AddSvg} alt="ADD" />
+                              </Button>
+                         </div>
+                    </Form>
+               </div>
           </div>
      );
 };

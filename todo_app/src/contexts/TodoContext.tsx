e751baeb46554cf382dabb87321a2f2ac663986f
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-26 12:08:35 +0800

    Update TodoContext.tsx

--- todo_app/src/contexts/TodoContext.tsx
+++ todo_app/src/contexts/TodoContext.tsx
@@ -13,6 +13,9 @@ interface Todo {
 interface TodoContextValue {
      get_todos: () => Promise<void>;
      todos: Todo[];
+     new_todo: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+     task_title: string;
+     setTaskTitle: (task_title: string) => void;
 } // define the TodoContextValue interface
 
 interface Props {
@@ -24,6 +27,7 @@ export const TodoContext = createContext<TodoContextValue | null>(null); // set 
 export const TodoProvider: React.FC<Props> = ({ children }: Props) => {
      let authContext = useContext(AuthContext);
      const [todos, setTodos] = useState<Todo[]>([]); // set the initial state of Todos to null
+     const [task_title, setTaskTitle] = useState(""); // set the initial state of task_title to null
 
      const get_todos = async () => {
           const request_instance = axios.create({
@@ -45,9 +49,39 @@ export const TodoProvider: React.FC<Props> = ({ children }: Props) => {
           } // if the response is successful, set the todos to the response data
      };
 
-     const new_todo = async (task_title: string) => {};
+     const new_todo = async (e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
 
-     const contextData = { get_todos, todos }; // set the context data
+          const request_instance = axios.create({
+               headers: {
+                    Authorization: `Bearer ${authContext?.authTokens?.access}`,
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+               },
+          }); // create an axios instance with the auth token
+
+          const response = await request_instance.post(
+               "http://127.0.0.1:8000/tasks/",
+               {
+                    task_title: e.currentTarget.task_title.value,
+               },
+          ); // get the todos from the API
+
+          if (response.status === 200 || 201) {
+               get_todos();
+               setTaskTitle("");
+          } else {
+               console.log(response.status);
+          } // if the response is successful, set the todos to the response data
+     };
+
+     const contextData = {
+          get_todos,
+          todos,
+          new_todo,
+          task_title,
+          setTaskTitle,
+     }; // set the context data
 
      useEffect(() => {
           get_todos();

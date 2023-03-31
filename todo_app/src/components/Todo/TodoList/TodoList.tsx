7d93584151b9ce1d45ddea43912dfad3d798769d
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-03-31 19:40:19 +0800

    Update TodoList.tsx

--- todo_app/src/components/Todo/TodoList/TodoList.tsx
+++ todo_app/src/components/Todo/TodoList/TodoList.tsx
@@ -1,57 +1,148 @@
 import React, { useContext } from "react";
 
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
+import TodoForm from "../TodoForm/TodoForm";
 
 import { TodoContext } from "../../../contexts/TodoContext";
 
-import { EditSvg, DeleteSvg } from "../../Svgs";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+
+import { ToastContainer } from "react-toastify";
+
+import { EditSvg, DeleteSvg, CheckSvg } from "../../Svgs";
+
+import ListStyle from "../TodoList/Todo.module.css";
 
 const TodoList = () => {
      let todoContext = useContext(TodoContext);
+
      const completedTodos = todoContext?.todos.filter(
+          (todo) => todo.is_completed === true,
+     );
+
+     const unFinishedTodos = todoContext?.todos.filter(
           (todo) => todo.is_completed === false,
      );
 
      return (
           <>
-               {completedTodos?.map((todo) => (
-                    <ListGroup variant="flush" key={todo.task_id}>
-                         <ListGroup.Item className="border-bottom border-secondary-subtle text-align-middle">
-                              <div className="d-flex justify-content-between align-items-center">
-                                   <span>{todo.task_title}</span>
-                                   <ButtonGroup>
-                                        <Button
-                                             variant="primary"
-                                             onClick={(e) =>
-                                                  todoContext?.edit_todo(
-                                                       e,
+               <div className="d-flex justify-content-center align-items-center">
+                    <div className="col-lg-5 ">
+                         <ToastContainer
+                              hideProgressBar={true}
+                              toastStyle={{
+                                   fontSize: "15px",
+                                   fontWeight: "500",
+                                   letterSpacing: "1.5px",
+                              }}
+                         />
+                         <TodoForm />
+                         <Tabs
+                              className="nav-fill nav-tabs"
+                              defaultActiveKey="task-list"
+                              id="tab">
+                              <Tab
+                                   tabClassName="Completed Tasks"
+                                   className={`${ListStyle.todoContainer}`}
+                                   eventKey="task-list"
+                                   title="Tasks">
+                                   {unFinishedTodos?.map((todo) => (
+                                        <ListGroup
+                                             variant="flush"
+                                             key={todo.task_id}>
+                                             <ListGroup.Item className="border-bottom border-secondary-subtle text-align-middle">
+                                                  <div className="d-flex justify-content-between align-items-center">
+                                                       <span>
+                                                            {todo.task_title}
+                                                       </span>
+                                                       <ButtonGroup>
+                                                            <Button variant="success">
+                                                                 <img
+                                                                      src={
+                                                                           CheckSvg
+                                                                      }
+                                                                      alt="Edit"
+                                                                 />
+                                                            </Button>
+                                                            <Button
+                                                                 variant="primary"
+                                                                 onClick={(e) =>
+                                                                      todoContext?.edit_todo(
+                                                                           e,
 
-                                                       todo.task_id,
-                                                       todo.task_title,
-                                                  )
-                                             }>
-                                             <img src={EditSvg} alt="Edit" />
-                                        </Button>
-                                        <Button
-                                             variant="danger"
-                                             onClick={(e) =>
-                                                  todoContext?.delete_todo(
-                                                       e,
-                                                       todo.task_id,
-                                                  )
-                                             }>
-                                             <img
-                                                  src={DeleteSvg}
-                                                  alt="Delete"
-                                             />
-                                        </Button>
-                                   </ButtonGroup>
-                              </div>
-                         </ListGroup.Item>
-                    </ListGroup>
-               ))}
+                                                                           todo.task_id,
+                                                                           todo.task_title,
+                                                                      )
+                                                                 }>
+                                                                 <img
+                                                                      src={
+                                                                           EditSvg
+                                                                      }
+                                                                      alt="Edit"
+                                                                 />
+                                                            </Button>
+                                                            <Button
+                                                                 variant="danger"
+                                                                 onClick={(e) =>
+                                                                      todoContext?.delete_todo(
+                                                                           e,
+                                                                           todo.task_id,
+                                                                      )
+                                                                 }>
+                                                                 <img
+                                                                      src={
+                                                                           DeleteSvg
+                                                                      }
+                                                                      alt="Delete"
+                                                                 />
+                                                            </Button>
+                                                       </ButtonGroup>
+                                                  </div>
+                                             </ListGroup.Item>
+                                        </ListGroup>
+                                   ))}
+                              </Tab>
+                              <Tab
+                                   className={`${ListStyle.todoContainer}`}
+                                   eventKey="task-list-completed"
+                                   title="Completed Tasks">
+                                   {completedTodos?.map((todo) => (
+                                        <ListGroup
+                                             variant="flush"
+                                             key={todo.task_id}>
+                                             <ListGroup.Item className="border-bottom border-secondary-subtle text-align-middle">
+                                                  <div className="d-flex justify-content-between align-items-center">
+                                                       <span>
+                                                            {todo.task_title}
+                                                       </span>
+                                                       <ButtonGroup>
+                                                            <Button
+                                                                 variant="danger"
+                                                                 onClick={(e) =>
+                                                                      todoContext?.delete_todo(
+                                                                           e,
+                                                                           todo.task_id,
+                                                                      )
+                                                                 }>
+                                                                 <img
+                                                                      src={
+                                                                           DeleteSvg
+                                                                      }
+                                                                      alt="Delete"
+                                                                 />
+                                                            </Button>
+                                                       </ButtonGroup>
+                                                  </div>
+                                             </ListGroup.Item>
+                                        </ListGroup>
+                                   ))}
+                              </Tab>
+                         </Tabs>
+                    </div>
+               </div>
           </>
      );
 };
